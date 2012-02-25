90387b6dff36fb68d735bd8ac6350a9ae069f380
yama <yamamoto@kyms.ne.jp>
2012-02-25 14:36:57 +0900

    TinyMCEの挙動をhtmlareaと合わせる(改行操作など)

--- admin/includes/tinymce.php
+++ admin/includes/tinymce.php
@@ -14,9 +14,10 @@ foreach($languages as $a)
 if(array_search('ja',$lang_code)!==false) $language = 'ja';
 else                                      $language = 'en';
 
-$plugins = 'advlist,save,autolink,lists,style,fullscreen,advimage,paste,advlink,media,contextmenu,table,advhr,inlinepopups';
+$plugins = 'advlist,save,autolink,lists,fullscreen,advimage,paste,advlink,media,contextmenu,table,advhr,inlinepopups';
 $theme_advanced_buttons1  = 'fontselect,fontsizeselect,formatselect,bold,italic,underline,strikethrough,|,sup,sub,|,copy,cut,pastetext,pasteword';
 $theme_advanced_buttons2  = 'justifyleft,justifycenter,justifyright,justifyfull,|,numlist,bullist,outdent,indent,|,forecolor,backcolor,|,advhr,link,image,media,table,code,|,fullscreen,|,help';
+$site_url = HTTP_CATALOG_SERVER . DIR_WS_CATALOG;
 $base_dir = DIR_WS_CATALOG;
 //<!-- load the main TinyMCE files -->
 $str  = '';
@@ -27,6 +28,16 @@ $str .= "theme : 'advanced',\n";
 $str .= "mode : 'textareas',\n";
 $str .= "language : '{$language}',\n";
 $str .= "plugins : '{$plugins}',\n";
+$str .= "accessibility_warnings            : false,\n";
+$str .= "document_base_url                 : '{$site_url}',\n";
+$str .= "relative_urls                     : false,\n";
+$str .= "remove_script_host                : false,\n";
+$str .= "convert_urls                      : true,\n";
+$str .= "force_br_newlines                 : true,\n";
+$str .= "force_p_newlines                  : false,\n";
+$str .= "forced_root_block                 : '',\n";
+$str .= "convert_fonts_to_spans            : true,\n";
+$str .= "valid_elements : '*[*]',\n";
 $str .= "theme_advanced_buttons1 : '{$theme_advanced_buttons1}',\n";
 $str .= "theme_advanced_buttons2 : '{$theme_advanced_buttons2}',\n";
 $str .= "theme_advanced_buttons3 : '',\n";
